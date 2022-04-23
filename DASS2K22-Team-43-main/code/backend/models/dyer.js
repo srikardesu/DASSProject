47b741ce4afdb75a75a8f5d9807e2f9e248ae3ca
Radheshyam23 <23radheshyam@gmail.com>
2022-04-23 12:27:10 +0530

    Made some parameters not compulsory for dyer during registration

--- DASS2K22-Team-43-main/code/backend/models/dyer.js
+++ DASS2K22-Team-43-main/code/backend/models/dyer.js
@@ -8,8 +8,8 @@ const dyerSchema = new Schema({
     password: { type: String, required: true },
     contact_no: { type: String, required: true },
     address: { type: String, required: true },
-    manager_name: { type: String, required: true },
-    date_of_joining: { type: String, required: true }
+    manager_name: { type: String},
+    date_of_joining: { type: String}
 });
 
 const dyer = mongoose.model('dyer', dyerSchema);
